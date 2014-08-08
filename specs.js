ff9f67f4a3f5b8abd22feeb1644c0fdbbae63162
xsimov <xsimov@gmail.com>
2014-08-08 20:21:28 +0200

    Kata finished

--- specs.js
+++ specs.js
@@ -1,4 +1,5 @@
 describe("The string calculator", function(){
+
     it("accepts a string and outputs a number", function() {
         expect(typeof calculate("")).toBe("number");
     });
@@ -9,6 +10,18 @@ describe("The string calculator", function(){
 
     it("calculates '1-1' and returns 0", function() {
       expect(calculate('1-1')).toBe(0);
+    });
+
+    it("calculates 1/1 and returns 1", function() {
+      expect(calculate('1/1')).toBe(1);
+    });
+
+    it("calculates 1*2 and returns 2", function() {
+      expect(calculate('1*2')).toBe(2);
+    });
+
+    it("calculates 10+2 and returns 12", function() {
+      expect(calculate('10+2')).toBe(12);
     });
 
 });
--- stringCalculator.js
+++ stringCalculator.js
@@ -1,12 +1,50 @@
 function calculate(operation) {
-  var separatedOperation = operation.split('');
-  var firstOperand = parseInt(separatedOperation[0]);
-  var secondOperand = parseInt(separatedOperation[operation.length - 1]);
 
-  if (isNaN(firstOperand) || isNaN(secondOperand)) {
+  var separatedEntry = operation.split('');
+  var numbers = ['',''];
+  var counter = 0;
+
+  separatedEntry.forEach(function(i){
+    if (isNaN(i)){
+      counter++;
+      operationSymbol = i;
+    }else{
+      numbers[counter] += i;
+    }
+  });
+
+  numbers[0] = parseInt(numbers[0]);
+  numbers[1] = parseInt(numbers[1]);
+
+  if (isNaN(numbers[0]) || isNaN(numbers[1])) {
     return NaN;
   };
 
-  return firstOperand + secondOperand;
+  function sum(a,b,c){
+    return a + c[b - 1];
+  }
 
+  function extract(a,b,c){
+    return a - c[b - 1];
+  }
+
+  function divide(a,b,c){
+    return a / c[b - 1];
+  }
+
+  function multiply(a,b,c){
+    return a * c[b - 1];
+  }
+
+  allOperations = {
+    "+": sum,
+    "-": extract,
+    "/": divide,
+    "*": multiply
+  };
+
+  var operationToDo = allOperations[operationSymbol];
+
+  result = numbers.map(operationToDo);
+  return result[result.length - 1];
 }
