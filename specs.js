608e8420d735077a871ad8fec38d471caefdacd4
Oscar <oscarbc@me.com>
2014-08-08 17:19:36 +0200

    Red

--- specs.js
+++ specs.js
@@ -2,4 +2,7 @@ describe("The string calculator", function(){
     it("accepts a string and outputs a number", function() {
         expect(typeof calculate("")).toBe("number");
     });
+    it("calculate '1+1' and returns 2", function() {
+    	expect(calculate("1+1")).toBe(2);
+    });
 });
