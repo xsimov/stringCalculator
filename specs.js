c227443a29b65e785c45b7777d36f4e198a0dcd1
xsimov <xsimov@gmail.com>
2014-08-08 17:35:33 +0200

    red

--- specs.js
+++ specs.js
@@ -2,7 +2,13 @@ describe("The string calculator", function(){
     it("accepts a string and outputs a number", function() {
         expect(typeof calculate("")).toBe("number");
     });
+
     it("calculate '1+1' and returns 2", function() {
     	expect(calculate("1+1")).toBe(2);
     });
+
+    it("calculates '1-1' and returns 0", function() {
+      expect(calculate('1-1')).toBe(0);
+    });
+
 });
--- stringCalculator.js
+++ stringCalculator.js
@@ -1,3 +1,12 @@
 function calculate(operation) {
-	return NaN;
+  var separatedOperation = operation.split('');
+  var firstOperand = parseInt(separatedOperation[0]);
+  var secondOperand = parseInt(separatedOperation[operation.length - 1]);
+
+  if (isNaN(firstOperand) || isNaN(secondOperand)) {
+    return NaN;
+  };
+
+  return firstOperand + secondOperand;
+
 }
